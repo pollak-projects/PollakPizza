273be45e079b77160f3648a7f5c126060787ffc8
MrGaben <hgabor2005@gmail.com>
2025-03-25 11:59:18 +0100

    no console log

--- backend/routes/allsizes.js
+++ backend/routes/allsizes.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models/db');
+const db = require("../models/db");
 
-router.get('/allsizes', (req, res) => {
-    const query = `
+router.get("/allsizes", (req, res) => {
+  const query = `
         SELECT *
         FROM size 
     `;
 
-    db.query(query)
-    .then(results => {
-      console.log(results[0])
+  db.query(query)
+    .then((results) => {
       res.json(results[0]);
     })
-    .catch(err => {
-      console.error('Database query error:', err); // Log the error
-      res.status(500).json({ error: 'Hiba a méretek lekérdezése során.' });
+    .catch((err) => {
+      console.error("Database query error:", err); // Log the error
+      res.status(500).json({ error: "Hiba a méretek lekérdezése során." });
     });
 });
-
 
 module.exports = router;
